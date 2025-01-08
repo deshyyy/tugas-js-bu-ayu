f5dc69a25b8fb3168d75e4f7441072a20981d4b2
deshyyy <desywahrcput@gmail.com>
2025-01-08 12:23:12 +0700

    Add files via upload

---
+++ variabel.js
@@ -0,0 +1,22 @@
+let nama = "Desy Wahyu Rachma Putri";
+let alamat = "Dempet Tugu Sayung Demak";
+let usia = "16 Tahun";
+let TTL = "Demak,14 Desember 2009";
+let gender = "Perempuan";
+let agama = "Islam";
+let email = "desywahrcput@gmail.com";
+let berat = "85kg";
+let tinggi = "157cm";
+let hobi ="hiyak hiyakk";
+let cita = "ingin menjadi orang yang sukses";
+let stasus = "Pelajar";
+let sekolah = "SMKN 1 Sayung";
+let jurusan = "PPLG";
+
+document.write("nama saya adalah"+ nama + "saya tinggal di"+ alamat + "pada tahun 2025 ini usia saya adalah"+ usia +
+    "saya lahir di"+ TTL + "jenis kelamin saya adalah"+ gender + "saya beragama"+ agama + "alamat email saya"+ email +
+    "saya memiliki berat badan"+ berat + "dengan tinggi"+ tinggi + "saya sangat senang"+ hobi +
+    "karena saya memiliki cita cita"+ cita + "saat ini saya adalah seorang"+ status + "yang bersekolah di"+ sekolah +
+    "pada jurusan"+ jurusan)
+
+
